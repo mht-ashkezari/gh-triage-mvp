869dd9fb4e308583a6245d27be882cd1c0e8ec38
mht-ashkezari <mht.ashkezari@gmail.com>
2025-10-05 12:37:58 +0200

    build(schemas): add .js extension in re-exports for NodeNext; declare schemas dep in contracts for runtime import

--- packages/schemas/src/index.ts
+++ packages/schemas/src/index.ts
@@ -1,6 +1,10 @@
 
+export { CriticalFactsV1, TriageCardV1, ReleaseNotesV1 } from "./entities.js";
 
-export * from "./entities";
-export * from "./common";
+export * from "./entities.js";
+export * from "./common.js";
+
+export * as entities from "./entities.js";
+export * as common from "./common.js";
 // central re-exports; makes imports shorter
-export * from "./datasets/snapshot";
+export * from "./datasets/snapshot.js";
